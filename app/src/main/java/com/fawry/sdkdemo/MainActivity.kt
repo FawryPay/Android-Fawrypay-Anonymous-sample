3ebe7050c7619dcd311b73e4a9b24636866869ef
abdelrahman.khater@fawry.com <abdelrahman.khater@fawry.com>
2025-08-10 11:15:23 +0300

    updated the sdk version

--- app/src/main/java/com/fawry/sdkdemo/MainActivity.kt
+++ app/src/main/java/com/fawry/sdkdemo/MainActivity.kt
@@ -93,7 +93,6 @@ class MainActivity : AppCompatActivity() {
                 payWithCardToken = true,
                 paymentMethods = PaymentMethods.ALL,
                 authCaptureMode = false,
-                signature = null,
             ),
             _callback = object : FawrySdkCallbacks {
                 override fun onSuccess(
